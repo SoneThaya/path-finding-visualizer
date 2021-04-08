cb4b7df1d48105f9085cea2b44ec2ad4dadb59f7
Sone <sthaya@gmail.com>
2021-04-07 18:12:14 -0700

    started path finding visualizer project

---
+++ src/Node/Node.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './node.css'
+
+export class Node extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="node">
+                node
+            </div>
+        )
+    }
+}
+
+export default Node
---
+++ src/components/PathFindingVisualizer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Node from '../Node/Node';
+import './pathFindingVisualizer.css'
+
+class PathFindingVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                foo
+                 <Node />
+            </div>
+        );
+    }
+}
+
+export default PathFindingVisualizer;
