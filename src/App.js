e2086f2de7199a36d4aa7049eacb0a4083dde1f2
Sone <sthaya@gmail.com>
2021-04-07 18:33:01 -0700

    completed path finding visualizer with dijkstar algorithm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import PathFindingVisualizer from './components/PathFindingVisualizer';
+import "./App.css";
+import PathFindingVisualizer from "./components/PathFindingVisualizer";
 
 function App() {
   return (
