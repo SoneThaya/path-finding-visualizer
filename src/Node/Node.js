16a62450f36f483e6b669c2fb7d1b7126c2c2991
Sone <sthaya@gmail.com>
2021-04-07 18:20:37 -0700

    worked on path finding file

--- src/Node/Node.js
+++ src/Node/Node.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './node.css'
+import "./node.css";
 
 export class Node extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div className="node">
-                node
-            </div>
-        )
-    }
+  render() {
+    return <div className="node"></div>;
+  }
 }
 
-export default Node
+export default Node;
